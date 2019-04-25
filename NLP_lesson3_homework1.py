73c482ba9fbc2cc5f849b1533c23a72e5703d0b2
GraceXYJ <18601131903@163.com>
2019-04-26 07:52:09 +0800

    first

--- NLP_lesson3_homework1.py
+++ NLP_lesson3_homework1.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import random
-ontent = pd.read_csv('/Users/mqgao/Workspace/Lecture/datasource/titanic/train.csv')
+content = pd.read_csv('/Users/mqgao/Workspace/Lecture/datasource/titanic/train.csv')
 content = content.dropna()
 age_with_fares = content[
     (content['Age'] > 22) & (content['Fare'] < 400) & (content['Fare'] > 130)
