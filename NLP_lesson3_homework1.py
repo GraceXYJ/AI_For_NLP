442f0abf17c69ec01327896c5175de123f95c68e
GraceXYJ <18601131903@163.com>
2019-04-26 09:59:32 +0800

    first

--- NLP_lesson3_homework1.py
+++ NLP_lesson3_homework1.py
@@ -2,10 +2,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import random
-content = pd.read_csv('/Users/mqgao/Workspace/Lecture/datasource/titanic/train.csv')
+
+
+content = pd.read_csv('E:/pythonNotebook/NLPLESSON3/Tatanic_data/train.csv')
 content = content.dropna()
 age_with_fares = content[
     (content['Age'] > 22) & (content['Fare'] < 400) & (content['Fare'] > 130)
 ]
 sub_fare = age_with_fares['Fare']
 sub_age = age_with_fares['Age']
+plt.scatter(sub_age,sub_fare)
+plt.show()
+def func(age, k, b):
+
+
